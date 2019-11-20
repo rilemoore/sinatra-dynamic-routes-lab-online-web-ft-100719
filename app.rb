666af8c5cd8ea9f2cdbcd97fdf1e8457433400fa
Riley Moore <rilemoore@gmail.com>
2019-11-20 01:03:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,6 +25,8 @@ class App < Sinatra::Base
   end
   
   get "/:operation/:number1/:number2" do
+    @number1 = params[:number1].to_i
+    
     if(params[:operation] == "add")
       "#{params[:number1].to_i + params[:number2].to_i}"
     end
