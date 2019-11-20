b8c531b8cbaebf1efda163741dc2172b16f138a2
Riley Moore <rilemoore@gmail.com>
2019-11-20 00:41:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
-  get "/hello/:name" do
+  get "/reversename/:name" do
     @user_name = params[:name]
     "Hello #{@user_name}!"
   end
